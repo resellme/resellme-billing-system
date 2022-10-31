dcd74565ae0d93bfa34c6f5801dcafa56e0b4701
Reza <privyreza@gmail.com>
2022-11-01 00:09:37 +0200

    Resized logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ \App\Models\Setting::where('name', 'logo')->first() ? \App\Models\Setting::where('name', 'logo')->first()->value : ''}}">
+    <img  width="200" src="{{ \App\Models\Setting::where('name', 'logo')->first() ? \App\Models\Setting::where('name', 'logo')->first()->value : ''}}">
 </a>
